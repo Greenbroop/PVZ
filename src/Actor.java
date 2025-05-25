5b7f3abdf164cfc89386ceb94e70d0ea4e539a68
Greenbroop <bongden0329@gmail.com>
2025-05-26 00:25:19 +0700

    First commit

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,14 @@
 public abstract class Actor{
     protected int health;
 
-    //Abstract method
+    //Khai báo lớp trừu tượng
     public abstract void attack();
 
     public void hit(int damage){
         health-=damage;
     }
 
-    //check is Actor dead
+    //Kiểm tra zombie chết chưa
     public boolean isDead(){
         return health<=0;
     }
